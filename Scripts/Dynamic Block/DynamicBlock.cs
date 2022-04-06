53ae9d672a53175b58fe9c54838ef9b076c355fc
chauAlex <apchau@ucsb.edu>
2022-04-05 23:33:27 -0700

    WIP: DynamicBlock, Instantiate is returning null

--- Scripts/Dynamic Block/DynamicBlock.cs
+++ Scripts/Dynamic Block/DynamicBlock.cs
@@ -7,19 +7,15 @@ using DefaultNamespace;
 
 public class DynamicBlock : MonoBehaviour
 {
+    public static DynamicBlock Instance { get; private set; }
+
+    [SerializeField] private WallInfoObject wallInfo;
+    public string collName;
     //private string prevRow;
     //private string[] obstaclesList = { "B1", "B3", "B5", "B6", "D2", "D3", "D5", "D6", "F2", "F4", "F5", "F7" };
-    [SerializeField] private WallInfoObject info;
 
-    private void OnEnable()
-    {
-        foreach (var item in info.GetPositions())
-        {
-            Debug.Log(item);
-        }
-    }
-
-    //[SerializeField] private GameObject[] walls; // 0 is north, 1 is south, 2 is east, 3 is west
+    [SerializeField] private GameObject[] walls; // 0 is north, 1 is south, 2 is east, 3 is west
+    private List<GameObject> possWalls;
     //[SerializeField] private BlockInfoObject blockInfo;
     //private Trial trial;
     //private Dictionary<GridLocation, Dictionary<GridLocation, List<Coordinate>>> wallLocations = new();
@@ -28,6 +24,10 @@ public class DynamicBlock : MonoBehaviour
     //private WallDirection wallDirection;
     //private GameObject wall;
 
+    private void OnEnable()
+    {
+        CreateWallTriggers();
+    }
     //private void OnEnable()
     //{
     //    blockLocations = new();
@@ -63,6 +63,11 @@ public class DynamicBlock : MonoBehaviour
     //    }
     //}
 
+    /*private int IndexOf(List<GameObject> goList, GridLocation target)
+    {
+        if()
+    }
+    */
     //private int IndexOf(List<GridLocation> list, GridLocation target)
     //{
     //    for (int i = 0; i < list.Count; i++)
@@ -107,7 +112,39 @@ public class DynamicBlock : MonoBehaviour
     //    }
     //}
 
-    //private void CreateNodeTriggers()
+    private void CreateWallTriggers()
+    {
+        GameObject temp;
+        for (int i = 0; i < wallInfo.GetPositions().Count; i++)
+        {
+            WallDirection dir = wallInfo.GetDirections()[i];
+            switch (dir)
+            {
+                case WallDirection.Vertical:
+                    temp = Instantiate(walls[2], new Vector3(wallInfo.GetPositions()[i].GetX(), 1, wallInfo.GetPositions()[i].GetY()), walls[2].transform.rotation) as GameObject;
+                    temp.name = wallInfo.GetPositions()[i].GetString() + "E";
+                    //temp.GetComponent<MeshRenderer>().enabled = false;
+                    possWalls.Add(temp);
+                    temp = Instantiate(walls[3], new Vector3(wallInfo.GetPositions()[i].GetX(), 1, wallInfo.GetPositions()[i].GetY()), walls[3].transform.rotation) as GameObject;
+                    //temp.GetComponent<MeshRenderer>().enabled = false;
+                    temp.name = wallInfo.GetPositions()[i].GetString() + "W";
+                    possWalls.Add(temp);
+                    break;
+                case WallDirection.Horizontal:
+                    temp = Instantiate(walls[0], new Vector3(wallInfo.GetPositions()[i].GetX(), 1, wallInfo.GetPositions()[i].GetY()), walls[0].transform.rotation) as GameObject;
+                    temp.name = wallInfo.GetPositions()[i].GetString() + "N";
+                    //temp.GetComponent<MeshRenderer>().enabled = false;
+                    possWalls.Add(temp);
+                    temp = Instantiate(walls[1], new Vector3(wallInfo.GetPositions()[i].GetX(), 1, wallInfo.GetPositions()[i].GetY()), walls[1].transform.rotation) as GameObject;
+                    temp.name = wallInfo.GetPositions()[i].GetString() + "S";
+                    //temp.GetComponent<MeshRenderer>().enabled = false;
+                    possWalls.Add(temp);
+                    break;
+            }
+        }
+    }
+
+    //private void CreatewallInfo.positions[i]Triggers()
     //{
     //    foreach (GridLocation node in blockLocations)
     //    {
@@ -131,51 +168,16 @@ public class DynamicBlock : MonoBehaviour
     //    }
     //}
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    if (ExperimentController.Instance.GetTrialInfo().stressTrial)
-    //    {
-    //        //Currently this will just compare where the start and end are relative to each other and determine what the row right
-    //        //before the end is. TODO: Determine an effective method for when start and end are on the same row, as for now it defaults
-    //        //to the row after it (this can be an issue if start is mailbox and end is chair for example as it goes out of bounds to Row H)
-    //        if (ExperimentController.Instance.GetTrialInfo().start.GetY().CompareTo(ExperimentController.Instance.GetTrialInfo().end.GetY()) < 0)
-    //        {
-    //            prevRow = (char)(ExperimentController.Instance.GetTrialInfo().end.y[0] - 1) + "";
-    //        }
-    //        else
-    //        {
-    //            prevRow = (char)(ExperimentController.Instance.GetTrialInfo().end.y[0] + 1) + "";
-    //        }
-
-    //        //if end is to your left it will try to block your left
-    //        //if end is to your right it will try to block to your right
-    //        //if end is ahead, it will try blocking top and bottom
-    //        //if left/right were unsuccessful, it will default to top/bottom
-    //        if (ControllerCollider.Instance.currentNode.y.Equals(prevRow))
-    //        {
-    //            //left case which reverts into top/bot
-    //            if(ExperimentController.Instance.GetTrialInfo().end.x < ControllerCollider.Instance.currentNode.x)
-    //            {
-    //                if (Array.IndexOf(obstaclesList, prevRow + (ControllerCollider.Instance.currentNode.GetX() - 1 + "")) == -1)
-    //                    ExperimentController.Instance.SetTrialBlockedLocation(ControllerCollider.Instance.currentNode.GetString().Substring(0,1), (int)ControllerCollider.Instance.currentNode.GetX() - 1);
-    //                else
-    //                    ExperimentController.Instance.SetTrialBlockedLocation(ExperimentController.Instance.GetTrialInfo().end.GetString().Substring(0,1), ExperimentController.Instance.GetTrialInfo().end.GetX());
-    //            } else if (ExperimentController.Instance.GetTrialInfo().end.GetX() > ControllerCollider.Instance.currentNode.GetX()) //right case
-    //            {
-    //                if (Array.IndexOf(obstaclesList, prevRow + (ControllerCollider.Instance.currentNode.x + 1 + "")) == -1)
-    //                    ExperimentController.Instance.SetTrialBlockedLocation(ControllerCollider.Instance.currentNode.GetString().Substring(0, 1), ControllerCollider.Instance.currentNode.GetX() + 1);
-    //                else
-    //                    ExperimentController.Instance.SetTrialBlockedLocation(ExperimentController.Instance.GetTrialInfo().end.GetString().Substring(0, 1), ExperimentController.Instance.GetTrialInfo().end.GetX());
-    //            } else //case that you are right in front of the end
-    //            {
-    //                ExperimentController.Instance.SetTrialBlockedLocation(ExperimentController.Instance.GetTrialInfo().end.GetString().Substring(0, 1), ExperimentController.Instance.GetTrialInfo().end.GetX());
-    //            }
-    //        }
-    //    }
-    //    else
-    //    {
-    //        this.gameObject.transform.position = new Vector3(12, 12, 12);
-    //    }
-    //}
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
