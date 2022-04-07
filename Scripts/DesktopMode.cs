6fb837e31e9ee538265d712aab2a0f4264c3b468
carolhe <carol.hcxy@gmail.com>
2022-04-07 11:45:47 -0700

    TestMerge

--- Scripts/DesktopMode.cs
+++ Scripts/DesktopMode.cs
@@ -12,19 +12,19 @@ public class DesktopMode : MonoBehaviour
     */
 
     [SerializeField] private StandaloneInputModule input;
-    [SerializeField] private CurvedUIInputModule curvedUI;
+   // [SerializeField] private CurvedUIInputModule curvedUI;
     
     void OnEnable()
     {
         if (ExperimentController.Instance.desktopMode)
         {
             input.enabled = true;
-            curvedUI.enabled = false;
+            //curvedUI.enabled = false;
         }
         else
         {
             input.enabled = false;
-            curvedUI.enabled = true;
+            //curvedUI.enabled = true;
         }
     }
 }
