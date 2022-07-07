36db8e54964f6450a260788989ea7918bbdddf4d
Justin <justin_kasowski@ucsb.edu>
2022-07-07 14:11:24 -0700

    Add missing files

--- Scripts/Classes/Constants.cs
+++ Scripts/Classes/Constants.cs
@@ -9,6 +9,6 @@ namespace Classes
         public const int GRID_LENGTH = 7;
         public const int GRID_WIDTH = 7;
         public static readonly string[] LETTERS = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        public const int NUM_OF_TRIALS = 26;
     }
 }
-
