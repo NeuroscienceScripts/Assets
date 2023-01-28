22e346b5eb30f1d0213f5d81e4ab7767974ae191
unity-sxr <116249808+unity-sXR@users.noreply.github.com>
2023-01-27 16:03:54 -0800

    incomplete fix

--- MazeHandler.cs
+++ MazeHandler.cs
@@ -6,23 +6,50 @@ using UnityEngine;
 
 public class MazeHandler : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI userText; 
+    [SerializeField] private TextMeshProUGUI userText;
+    [SerializeField] public Canvas StartCanvas;
+    [SerializeField] public Canvas UserCanvas;
+    [SerializeField] public GameObject Maze;
+    [SerializeField] public float startTime = 0.0f;
+    [SerializeField] public float timeTaken = 0.0f;
+    [SerializeField] private TMP_InputField subjectNum;
+    [SerializeField] public int subjectNo;
+    
     // Start is called before the first frame update
     void Start()
     {
-        userText.text = "Get to the yellow finish line"; 
+        StartCanvas.enabled = true;
+        UserCanvas.enabled = false;
+        Maze.SetActive(false);
+    }
+
+    public void Startup()
+    {
+        subjectNo = Int32.Parse((string) subjectNum.text.ToString());
+        if (subjectNo != 0)
+        {
+            StartCanvas.enabled = false;
+            UserCanvas.enabled = true;
+            Maze.SetActive(true);
+            userText.text = "Hold left click to move forward.\n Use the scroll-wheel to change the mouse sensitivity.\nGet to the yellow finish line";
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
-            userText.text = ""; 
+        if (Input.GetMouseButton(0) & startTime==0.0f)
+        {
+            userText.text = "";
+            startTime = Time.realtimeSinceStartup;
+        }
+
     }
 
     private void OnCollisionEnter(Collision other)
     {
         userText.text = "Congrats!";
+        timeTaken = Time.realtimeSinceStartup - startTime;
         Time.timeScale = 0;
     }
 }
