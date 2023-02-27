66b827b19fadc8e396aadbb8b0e592cd9a2fcbd4
unity-sxr <116249808+unity-sXR@users.noreply.github.com>
2023-02-27 13:26:34 -0800

    mouse training scene updates (added file logging)

--- MazeHandler.cs
+++ MazeHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Classes;
 using TMPro;
 using UnityEngine;
@@ -8,17 +9,20 @@ using VR;
 
 public class MazeHandler : MonoBehaviour
 {
+    public static MazeHandler Instance { get; private set; }
+    
     [SerializeField] private TextMeshProUGUI userText;
     [SerializeField] public Canvas StartCanvas;
     [SerializeField] public Canvas UserCanvas;
     [SerializeField] public GameObject Maze;
     [SerializeField] public float startTime = 0.0f;
-    [SerializeField] public float timeTaken = 0.0f;
+    [SerializeField] public float startTimeClick = 0.0f;
     [SerializeField] private TMP_InputField subjectNum;
     [SerializeField] public int subjectNo;
     [SerializeField] public GameObject player; 
     [SerializeField] private GameObject moveForwardArrow;
     public int stepInPhase = 0;
+    public FileHandler fileHandler = new FileHandler();
     
     private Vector3[] arrowPath =
     {
@@ -32,13 +36,18 @@ public class MazeHandler : MonoBehaviour
         new Vector3(-6.0f, -7.0f, 0.0f),
         new Vector3(0.0f, -7.0f, -90.0f)
     };
-    
+
+    public string Date_time;
+    public string subjectFile;
+    public int lap = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCanvas.enabled = true;
         UserCanvas.enabled = false;
         Maze.SetActive(false);
+        moveForwardArrow.SetActive(false);
     }
 
     public void Startup()
@@ -46,8 +55,14 @@ public class MazeHandler : MonoBehaviour
         float arrowHeight = moveForwardArrow.transform.position.y;
         player.GetComponent<SimpleFirstPersonMovement>().active = false;
         subjectNo = Int32.Parse((string) subjectNum.text.ToString());
+        Date_time = "_" + DateTime.Today.Month + "_" + DateTime.Today.Day + "-" + DateTime.Now.Hour + "_" + DateTime.Now.Minute;
         if (subjectNo != 0)
         {
+            Date_time = "_" + DateTime.Today.Month + "_" + DateTime.Today.Day + "-" + DateTime.Now.Hour + "_" + DateTime.Now.Minute;
+            subjectFile = Application.dataPath + "/" + "Data" + "/" + subjectNo + Date_time + ".csv";
+            fileHandler.AppendLine(
+                subjectFile.Replace(Date_time + ".csv",
+                    "_mouse_training.csv"), "StartTime,StepTime,Lap,StepInPhase");
             StartCanvas.enabled = false;
             UserCanvas.enabled = true;
             Maze.SetActive(true);
@@ -61,6 +76,8 @@ public class MazeHandler : MonoBehaviour
             player.GetComponent<SimpleFirstPersonMovement>().active = true;
             userText.text = "Left-click mouse to move forward.\n move mouse to left to turn left.\n move mouse forward to look up.\nUse the scroll-wheel to change the mouse sensitivity.";
         }
+
+        
     }
 
     private void Update()
@@ -71,20 +88,28 @@ public class MazeHandler : MonoBehaviour
             if (Input.GetMouseButton(0) & startTime==0.0f)
             {
                 startTime = Time.realtimeSinceStartup;
+                ExperimentController.Instance.recordCameraAndNodes = true;
             }
             
             if ((Mathf.Abs(player.transform.position.x - moveForwardArrow.transform.position.x) < 0.5f) && (Mathf.Abs(player.transform.position.z - moveForwardArrow.transform.position.z) < 0.5f))
             {
+                fileHandler.AppendLine(subjectFile.Replace(Date_time + ".csv",
+                    "_mouse_training.csv"),startTime.ToString() + ',' + Time.realtimeSinceStartup.ToString() + ',' + lap.ToString() + ',' + stepInPhase.ToString());
                 stepInPhase++;
-                moveForwardArrow.transform.position =
-                    new Vector3(arrowPath[stepInPhase].x, arrowHeight, arrowPath[stepInPhase].y);
-                moveForwardArrow.transform.rotation = Quaternion.Euler(
-                    moveForwardArrow.transform.rotation.eulerAngles.x,
-                    arrowPath[stepInPhase].z, moveForwardArrow.transform.rotation.eulerAngles.z);
+                if (stepInPhase < arrowPath.Length)
+                {
+                    moveForwardArrow.transform.position =
+                        new Vector3(arrowPath[stepInPhase].x, arrowHeight, arrowPath[stepInPhase].y);
+                    Debug.Log(stepInPhase);
+                    moveForwardArrow.transform.rotation = Quaternion.Euler(
+                        moveForwardArrow.transform.rotation.eulerAngles.x,
+                        arrowPath[stepInPhase].z, moveForwardArrow.transform.rotation.eulerAngles.z);
+                }
             }
         }
         else
         {
+            lap++;
             stepInPhase = 0;
             moveForwardArrow.transform.position =
                 new Vector3(arrowPath[stepInPhase].x, arrowHeight, arrowPath[stepInPhase].y);
@@ -93,6 +118,20 @@ public class MazeHandler : MonoBehaviour
                 arrowPath[stepInPhase].z, moveForwardArrow.transform.rotation.eulerAngles.z);
         }
         
+    }
+    
+    void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // private void OnCollisionEnter(Collision other)
--- Scripts/VR/CameraTracker.cs
+++ Scripts/VR/CameraTracker.cs
@@ -17,52 +17,92 @@ namespace VR
         // public Material pointShader;
         public Camera vrCamera;
         public float smoothMove = 80;
+        private bool headerWritten = false;
+        private bool headerWrittenMouse = false;
 
 
-        private void Start()
+        private void WriteHeader()
         {
+            Debug.Log(ExperimentController.Instance.subjectFile);
             fileHandler.AppendLine(
                 ExperimentController.Instance.subjectFile.Replace(ExperimentController.Instance.Date_time + ".csv",
                     "_camera_tracker.csv"), "Trial_ID,TrialTime,Phase,TrialNumber,StepInPhase,Start,End," +
                                             "CamRotX,CamRotY,CamRotZ,CamPosX,CamPosY,CamPosZ,ScreenGazeX,ScreenGazeY,WorldGazeX,WorldGazeY,WorldGazeZ");
+        }
+        
+        private void WriteHeaderMouse()
+        {
+            Debug.Log(MazeHandler.Instance.subjectFile);
+            fileHandler.AppendLine(
+                MazeHandler.Instance.subjectFile.Replace(MazeHandler.Instance.Date_time + ".csv",
+                    "_mouse_camera_tracker.csv"), "TrialTime,StepInPhase,CamRotX,CamRotY,CamRotZ,CamPosX,CamPosY,CamPosZ");
         }
 
         private void OnRenderImage(RenderTexture src, RenderTexture dest)
         {
             if (Time.time - recordHeadTimer > timeInterval & ExperimentController.Instance.recordCameraAndNodes)
             {
-                var provider = TobiiXR.Internal.Provider;
-                var eyeTrackingData = new TobiiXR_EyeTrackingData();
-                provider.GetEyeTrackingDataLocal(eyeTrackingData);
-                var smoothMoveSpeed = true;
+                
+                if (!GetComponent<SimpleFirstPersonMovement>().enabled)
+                {
+                    if (!headerWritten)
+                    {
+                        headerWritten = true; 
+                        WriteHeader();
+                    }
+                    var provider = TobiiXR.Internal.Provider;
+                    var eyeTrackingData = new TobiiXR_EyeTrackingData();
+                    provider.GetEyeTrackingDataLocal(eyeTrackingData);
+                    var smoothMoveSpeed = true;
 
-                var interpolatedGazeDirection = Vector3.Lerp(ExperimentController.Instance._lastGazeDirection, eyeTrackingData.GazeRay.Direction,
-                    (smoothMove) * Time.unscaledDeltaTime);
-                var usedDirection = smoothMoveSpeed
-                    ? interpolatedGazeDirection.normalized
-                    : eyeTrackingData.GazeRay.Direction.normalized;
-                ExperimentController.Instance._lastGazeDirection = usedDirection;
+                    var interpolatedGazeDirection = Vector3.Lerp(ExperimentController.Instance._lastGazeDirection, eyeTrackingData.GazeRay.Direction,
+                        (smoothMove) * Time.unscaledDeltaTime);
+                    var usedDirection = smoothMoveSpeed
+                        ? interpolatedGazeDirection.normalized
+                        : eyeTrackingData.GazeRay.Direction.normalized;
+                    ExperimentController.Instance._lastGazeDirection = usedDirection;
 
-                var screenPos =
-                    vrCamera.WorldToScreenPoint(vrCamera.transform.position +
-                                                vrCamera.transform.rotation * usedDirection);
+                    var screenPos =
+                        vrCamera.WorldToScreenPoint(vrCamera.transform.position +
+                                                    vrCamera.transform.rotation * usedDirection);
 
-                float gazeX = screenPos.x / src.width;
-                float gazeY = screenPos.y / src.height;
-
-                fileHandler.AppendLine(
-                    ExperimentController.Instance.subjectFile.Replace(ExperimentController.Instance.Date_time + ".csv",
-                        "_camera_tracker.csv"),
-                    ExperimentController.Instance.PrintStepInfo() + "," +
-                    ExperimentController.Instance.GetTrialInfo().ToString() +
-                    "," + gameObject.transform.rotation.eulerAngles.x.ToString() + "," +
-                    gameObject.transform.rotation.eulerAngles.y.ToString() +
-                    "," + gameObject.transform.rotation.eulerAngles.z.ToString() + "," +
-                    gameObject.transform.position.x.ToString() + "," +
-                    gameObject.transform.position.y.ToString() + "," + gameObject.transform.position.z.ToString() +
-                    "," +
-                    gazeX + "," + gazeY + "," + usedDirection.x + "," + usedDirection.y + "," + usedDirection.z);
-                recordHeadTimer = Time.time;
+                    float gazeX = screenPos.x / src.width;
+                    float gazeY = screenPos.y / src.height;
+                    
+                    fileHandler.AppendLine(
+                        ExperimentController.Instance.subjectFile.Replace(ExperimentController.Instance.Date_time + ".csv",
+                            "_camera_tracker.csv"),
+                        ExperimentController.Instance.PrintStepInfo() + "," +
+                        ExperimentController.Instance.GetTrialInfo().ToString() +
+                        "," + gameObject.transform.rotation.eulerAngles.x.ToString() + "," +
+                        gameObject.transform.rotation.eulerAngles.y.ToString() +
+                        "," + gameObject.transform.rotation.eulerAngles.z.ToString() + "," +
+                        gameObject.transform.position.x.ToString() + "," +
+                        gameObject.transform.position.y.ToString() + "," + gameObject.transform.position.z.ToString() +
+                        "," +
+                        gazeX + "," + gazeY + "," + usedDirection.x + "," + usedDirection.y + "," + usedDirection.z);
+                    recordHeadTimer = Time.time;
+                }
+                else
+                {
+                    if (!headerWrittenMouse)
+                    {
+                        headerWrittenMouse = true; 
+                        WriteHeaderMouse();
+                    }
+                    fileHandler.AppendLine(
+                        MazeHandler.Instance.subjectFile.Replace(MazeHandler.Instance.Date_time + ".csv",
+                            "_mouse_camera_tracker.csv"),
+                        Time.realtimeSinceStartup + "," + MazeHandler.Instance.stepInPhase +
+                        "," + gameObject.transform.rotation.eulerAngles.x.ToString() + "," +
+                        gameObject.transform.rotation.eulerAngles.y.ToString() +
+                        "," + gameObject.transform.rotation.eulerAngles.z.ToString() + "," +
+                        gameObject.transform.position.x.ToString() + "," +
+                        gameObject.transform.position.y.ToString() + "," + gameObject.transform.position.z.ToString());
+                    recordHeadTimer = Time.time;
+                }
+                
+                
 
             }
 
